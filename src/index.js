406bc616ab2d34575b97361d1449bc5b8cd56cb6
Spamwistz <125665991+Spamwistz@users.noreply.github.com>
2023-02-17 14:25:43 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,59 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./index.css";
 
-const API_URL = 'https://api.coincap.io/v2/assets';
-
-async function fetchData(url) {
-  const response = await fetch(url);
-  const data = await response.json();
-  return data.data;
-}
-
-function renderTable(data) {
-  const table = (
-    <table>
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>Name</th>
-          <th>Price</th>
-          <th>Change (24h)</th>
-          <th>Market Cap</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((coin, index) => (
-          <tr key={coin.id}>
-            <td>{index + 1}</td>
-            <td>{coin.name}</td>
-            <td>${(+coin.priceUsd).toFixed(2)}</td>
-            <td style={{ color: coin.changePercent24Hr < 0 ? 'red' : 'green' }}>
-              {coin.changePercent24Hr}%
-            </td>
-            <td>${(+coin.marketCapUsd).toLocaleString()}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  );
-
-  ReactDOM.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-    document.getElementById('root')
-  );
-}
-
-async function main() {
-  try {
-    const data = await fetchData(API_URL);
-    renderTable(data);
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-main();
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
